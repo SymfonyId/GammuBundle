ba92dc58d266e00297006a1f6fc74acd4979938d
ihsanudin <surya.kejawen@gmail.com>
2015-04-17 17:05:04 +0700

    add check config

--- DependencyInjection/SymfonianIndonesiaGammuExtension.php
+++ DependencyInjection/SymfonianIndonesiaGammuExtension.php
@@ -6,6 +6,8 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 
 class SymfonianIndonesiaGammuExtension extends Extension
 {
@@ -13,11 +15,16 @@ class SymfonianIndonesiaGammuExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+        $fileSystem = new Filesystem();
 
         $container->setParameter('symfonian_id.gammu.smsd_inject_path', $config['smsd_inject_path']);
         $container->setParameter('symfonian_id.gammu.gammu_path', $config['gammu_path']);
 
         foreach ($config['gammurc_path'] as $key => $gammurc) {
+            if ($fileSystem->exists($gammurc['path'])) {
+                throw new FileNotFoundException(sprintf('%s not found.'));
+            }
+
             if (! $container->hasParameter('symfonian_id.gammu.gammurc.default')) {
                 $container->setParameter('symfonian_id.gammu.gammurc.default', $gammurc['path']);
             }
@@ -26,6 +33,10 @@ class SymfonianIndonesiaGammuExtension extends Extension
         }
 
         foreach ($config['smsdrc_path'] as $key => $smsdrc) {
+            if ($fileSystem->exists($smsdrc['path'])) {
+                throw new FileNotFoundException(sprintf('%s not found.'));
+            }
+
             if (! $container->hasParameter('symfonian_id.gammu.smsdrc.default')) {
                 $container->setParameter('symfonian_id.gammu.smsdrc.default', $smsdrc['path']);
             }
